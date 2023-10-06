f8cfa732601e7557af196ed1c32c432d083bf5ef
teq0 <craig.hooper@gmail.com>
2023-10-06 23:23:21 +1100

    build: fix semrel plugins

--- release.config.js
+++ release.config.js
@@ -5,7 +5,6 @@ module.exports = {
     '@semantic-release/release-notes-generator',
     '@semantic-release/github',
     [
-      '@semantic-release/git',
       {
         assets: ['CHANGELOG.md', 'package.json'],
         message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
