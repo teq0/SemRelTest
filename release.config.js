c430dadbeaf2715afe330014baa4f7205d620689
teq0 <craig.hooper@gmail.com>
2023-10-06 23:11:06 +1100

    build: fix sem-rel config

--- release.config.js
+++ release.config.js
@@ -1,7 +1,15 @@
 module.exports = {
+  branches: ['main'],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/github'
+    '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+      }
+    ]
   ]
 };
