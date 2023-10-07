2770db8546f28a07e899b7108ccb2a61cab50d3b
teq0 <craig.hooper@gmail.com>
2023-10-08 08:27:01 +1100

    feat(api): use optimal zing
    Maximum zing was too much for some customers

--- src/SemRelTest.LambdaSQS/src/SemRelTest.LambdaSQS/Function.cs
+++ src/SemRelTest.LambdaSQS/src/SemRelTest.LambdaSQS/Function.cs
@@ -37,7 +37,7 @@ public class Function
 
     private async Task ProcessMessageAsync(SQSEvent.SQSMessage message, ILambdaContext context)
     {
-        context.Logger.LogInformation($"Processed message {message.Body} with maximum zing.");
+        context.Logger.LogInformation($"Processed message {message.Body} with optimum zing.");
         context.Logger.LogDebug($"Message ID: {message.MessageId}");
 
         // TODO: Do interesting work based on the new message
